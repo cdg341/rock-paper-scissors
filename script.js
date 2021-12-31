49f6f8f8d0b7f78f780bbba79df8816186078e19
Cameron <cdg341@gmail.com>
2021-12-31 01:59:07 -0600

    Play 5 rounds

--- script.js
+++ script.js
@@ -2,57 +2,50 @@
 
 function computerPlay(pcChoice) {
     const choices = ["rock", "paper", "scissors"];
-    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
-    return randomChoice;
+    return choices[Math.floor(Math.random() * choices.length)];
 }
-
 
 // Compares player choice vs computers choice
 
 function playRound(playerSelection, computerSelection) {
-    let result;
+    playerSelection = prompt("Rock - Paper - Scissors?").toLowerCase();
+    computerSelection = computerPlay();
 
     if (playerSelection === "rock" && computerSelection === "rock") {
-        result = ("Rock can't smash rock, It's a tie!");
+        return ("Rock can't smash rock, It's a tie!");
     }
     else if (playerSelection === "rock" && computerSelection === "scissors") {
-        result = ("Rock smashes scissors! You won, Congratulations!");
+        return ("Rock smashes scissors! You won, Congratulations!");
     }
     else if (playerSelection === "rock" && computerSelection === "paper") {
-        result = ("You lose, Paper wraps Rock!");
+        return ("You lose, Paper wraps Rock!");
     }
 
     if (playerSelection === "paper" && computerSelection === "rock") {
-        result = ("Paper covers rock, you won!");
+        return ("Paper covers rock, you won!");
     }
     else if (playerSelection === "paper" && computerSelection === "scissors") {
-        result = ("Sorry you lost, scissors cuts paper!");
+        return ("Sorry you lost, scissors cuts paper!");
     }
     else if (playerSelection === "paper" && computerSelection === "paper") {
-        result = ("It's a tie!");
+        return ("It's a tie!");
     }
 
     if (playerSelection === "scissors" && computerSelection === "rock") {
-        result = ("Rock smashes scissors, you lost!");
+        return ("Rock smashes scissors, you lost!");
     }
     else if (playerSelection === "scissors" && computerSelection === "scissors") {
-        result = ("It's a tie!");
+        return ("It's a tie!");
     }
     else if (playerSelection === "scissors" && computerSelection === "paper") {
-        result = ("Scissors cuts through paper, you won!");
+        return ("Scissors cuts through paper, you won!");
     }
-
-    return result;
 }
 
-const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-const computerSelection = computerPlay();
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound());
+    }
+}
 
-
-console.log(playRound(playerSelection, computerSelection));
-
-
-
-console.log("The computer chose " + computerSelection);
-
-
+game();
